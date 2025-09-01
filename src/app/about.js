395ff6dcf845fc2f178e10494eb3c61142fd4a88
Enzo <enzo.gouge@epitech.eu>
2025-09-01 14:43:30 +0200

    fix: parenthesis char into html parenthesis char

--- src/app/about.js
+++ src/app/about.js
@@ -5,63 +5,65 @@ import Image from "next/image";
 import WhiteStar from "../../public/white-star.svg";
 import { motion } from "motion/react";
 
-export function About(){
-    useEffect(()=>{
+export function About() {
+    useEffect(() => {
         const cvLink = document.getElementById("cv-link");
         const cvBtn = document.getElementById("cv-btn");
         const cvBg = document.getElementById("cv-bg");
-        cvBtn.addEventListener("mouseover", ()=>{
-            cvBg.className="-z-10  w-full h-full top-0 left-0 absolute bg-[#A79EA1] scale-y-100 transition";
+        cvBtn.addEventListener("mouseover", () => {
+            cvBg.className = "-z-10  w-full h-full top-0 left-0 absolute bg-[#A79EA1] scale-y-100 transition";
         })
-        cvBtn.addEventListener("mouseout", ()=>{
-            cvBg.className="-z-10  w-full h-full top-0 left-0 absolute bg-[#A79EA1] scale-y-0 transition";
+        cvBtn.addEventListener("mouseout", () => {
+            cvBg.className = "-z-10  w-full h-full top-0 left-0 absolute bg-[#A79EA1] scale-y-0 transition";
         })
     })
-    
-    return(
+
+    return (
         <div id="about" className="relative flex flex-col min-h-screen bg-black h-full overflow-hidden">
-            <Image alt="Star-circle icon" src={Star} className="absolute w-full h-full p-5"/>
+            <Image alt="Star-circle icon" src={Star} className="absolute w-full h-full p-5" />
             <div className="z-0 flex flex-col items-center align-center h-screen self-center">
                 <div className="flex flex-col justify-center items-center text-left h-screen gap-5 sm:gap-10 sm:p-10">
-                <motion.h1
-                initial={{opacity: 0, y: -50}}
-                whileInView={{opacity: 1, y: 0}}
-                animated={{opacity: 1, y:0}}
-                transition={{
-                    type: "spring",
-                    damping: 10,
-                    duration: 2,
-                    }}
-                viewport={{ once: true, amount: 0.5 }}
-                 className="text-6xl w-full text-left p-8">Mon parcours</motion.h1>
-                    <motion.p
-                    initial={{opacity: 0, x: 50}}
-                    whileInView={{opacity: 1, x: 0}}
-                    animated={{opacity: 1, x:0}}
-                    transition={{
-                        type: "spring",
-                        damping: 10,
-                        duration: 1,
+                    <motion.h1
+                        initial={{ opacity: 0, y: -50 }}
+                        whileInView={{ opacity: 1, y: 0 }}
+                        animated={{ opacity: 1, y: 0 }}
+                        transition={{
+                            type: "spring",
+                            damping: 10,
+                            duration: 2,
                         }}
-                    viewport={{ once: true, amount: 0.5 }}
-                     className="text-2xl lg:text-3xl self-center leading-10">
-                    Je m'appelle Enzo LE MOAL, développeur web en formation à la Web@cadémie by Epitech.  
-                    Après un baccalauréat en spécialité maths et NSI, j&apos;ai tenté une licence maths-info,
-                    mais l&apos;approche ne me convenait pas. 
-                    Après deux ans d&apos;échecs, j&apos;ai décidé d&apos;apprendre le développement web par moi-même, et c&apos;est pour cette raison que je me retrouve actuellement ici dans cette formation.
+                        viewport={{ once: true, amount: 0.5 }}
+                        className="text-6xl w-full text-left p-8">Mon parcours</motion.h1>
+                    <motion.p
+                        initial={{ opacity: 0, x: 50 }}
+                        whileInView={{ opacity: 1, x: 0 }}
+                        animated={{ opacity: 1, x: 0 }}
+                        transition={{
+                            type: "spring",
+                            damping: 10,
+                            duration: 1,
+                        }}
+                        viewport={{ once: true, amount: 0.5 }}
+                        className="text-2xl lg:text-3xl self-center leading-10">
+                        Je m&apos;appelle Enzo LE MOAL, d&eacute;veloppeur web en formation &agrave; la Web@cad&eacute;mie by Epitech.<br />
+                        Apr&egrave;s un baccalaur&eacute;at en sp&eacute;cialit&eacute; maths et NSI, j&apos;ai tent&eacute; une licence maths-info,
+                        mais l&apos;approche ne me convenait pas.<br />
+                        Apr&egrave;s deux ans d&apos;&eacute;checs, j&apos;ai d&eacute;cid&eacute; d&apos;apprendre le d&eacute;veloppement web par moi-m&ecirc;me, et c&apos;est pour cette raison que je me retrouve actuellement ici dans cette formation.
                     </motion.p>
                     <motion.div
-                    initial={{opacity: 0, x: -50}}
-                    whileInView={{opacity: 1, x: 0}}
-                    animated={{opacity: 1, x:0}}
-                    transition={{x:{
-                        type: "spring",
-                        damping: 10,
-                        duration: 1,
-                        }}}
-                    viewport={{ once: true, amount: 0.5 }}
-                     id="cv-btn" className="relative w-fit h-fit border-4 border-[#A79EA1] border-double hover:text-black ">
-                        <a id="cv-link" href="files/CV.pdf" download="EnzoLeMoal.pdf" className="z-100 flex gap-5 items-center text-lg lg:text-2xl cursor-pointer bg-transparent"><Image src={WhiteStar} alt="Star  logo" height={50}/>Télécharger mon CV<Image src={WhiteStar} alt="Star logo" height={50}/></a>
+                        initial={{ opacity: 0, x: -50 }}
+                        whileInView={{ opacity: 1, x: 0 }}
+                        animated={{ opacity: 1, x: 0 }}
+                        transition={{
+                            x: {
+                                type: "spring",
+                                damping: 10,
+                                duration: 1,
+                            }
+                        }}
+                        viewport={{ once: true, amount: 0.5 }}
+                        id="cv-btn" className="relative w-fit h-fit border-4 border-[#A79EA1] border-double hover:text-black ">
+                        <a id="cv-link" href="files/CV.pdf" download="EnzoLeMoal.pdf" className="z-100 flex gap-5 items-center text-lg lg:text-2xl cursor-pointer bg-transparent"><Image src={WhiteStar} alt="Star  logo" height={50} />Télécharger mon CV<Image src={WhiteStar} alt="Star logo" height={50} /></a>
                         <div id="cv-bg" className="-z-10 w-full h-full top-0 left-0 absolute bg-[#A79EA1] scale-y-0 transition"></div>
                     </motion.div>
                 </div>
@@ -70,30 +72,30 @@ export function About(){
             </div>
             <div className="z-0 flex flex-col items-center align-center self-center">
                 <div className="flex flex-col justify-center items-center text-left my-auto gap-5 sm:gap-10 sm:p-10">
-                <motion.h1
-                initial={{opacity: 0, y: -50}}
-                whileInView={{opacity: 1, y: 0}}
-                animated={{opacity: 1, y:0}}
-                transition={{
-                    type: "spring",
-                    damping: 10,
-                    duration: 2,
-                    }}
-                viewport={{ once: true, amount: 0.5 }}
-                 className="text-6xl w-full text-right p-8">Un morceau de moi...</motion.h1>
-                    <motion.p
-                    initial={{opacity: 0, x: 50}}
-                    whileInView={{opacity: 1, x: 0}}
-                    animated={{opacity: 1, x:0}}
-                    transition={{
-                        type: "spring",
-                        damping: 10,
-                        duration: 1,
+                    <motion.h1
+                        initial={{ opacity: 0, y: -50 }}
+                        whileInView={{ opacity: 1, y: 0 }}
+                        animated={{ opacity: 1, y: 0 }}
+                        transition={{
+                            type: "spring",
+                            damping: 10,
+                            duration: 2,
                         }}
-                    viewport={{ once: true, amount: 0.5 }}
-                     className="text-xl lg:text-2xl text-right self-center leading-10">
-                    Depuis petit je suis de nature créative, imaginative, je dessinais souvent, habitude que j'ai gardé malgré que ça n'arrive que rarement.
-                    Ensuite je me suis tourné petit à petit vers la musique, c'est probablement la chose que j'aime le plus, donc j'ai décidé de commencer à apprendre à jouer de la guitare (électrique).
+                        viewport={{ once: true, amount: 0.5 }}
+                        className="text-6xl w-full text-right p-8">Un morceau de moi...</motion.h1>
+                    <motion.p
+                        initial={{ opacity: 0, x: 50 }}
+                        whileInView={{ opacity: 1, x: 0 }}
+                        animated={{ opacity: 1, x: 0 }}
+                        transition={{
+                            type: "spring",
+                            damping: 10,
+                            duration: 1,
+                        }}
+                        viewport={{ once: true, amount: 0.5 }}
+                        className="text-xl lg:text-2xl text-right self-center leading-10">
+                        Depuis petit je suis de nature cr&eacute;ative, imaginative, je dessinais souvent, habitude que j&apos;ai gard&eacute; malgr&eacute; que &ccedil;a n&apos;arrive que rarement.<br />
+                        Ensuite je me suis tourn&eacute; petit &agrave; petit vers la musique, c&apos;est probablement la chose que j&apos;aime le plus, donc j&apos;ai d&eacute;cid&eacute; de commencer &agrave; apprendre &agrave; jouer de la guitare (&eacute;lectrique).
                     </motion.p>
                 </div>
                 <div>
