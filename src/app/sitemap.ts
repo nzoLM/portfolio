83093f693e2ac3d63d5486268d0f6b4a224ef765
nzoLM <enzo.gouge@epitech.eu>
2026-05-11 10:56:08 +0200

    feat: update sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -4,12 +4,14 @@ export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
       url: 'https://enzo-lemoal.fr',
-      lastModified: new Date(),
+      lastModified: new Date('2026-05-11'),
+      changeFrequency: 'monthly',
       priority: 1,
     },
     {
       url: 'https://enzo-lemoal.fr/competences',
-      lastModified: new Date(),
+      lastModified: new Date('2026-05-11'),
+      changeFrequency: 'yearly',
       priority: 0.8,
     },
   ]
